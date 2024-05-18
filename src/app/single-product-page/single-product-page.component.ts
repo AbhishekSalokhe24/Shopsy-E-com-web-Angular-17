0212b2ad21a79434c15f2c78fa91094a57fdcdff
AbhishekSalokhe24 <abhisaloke2424@gmail.com>
2024-05-18 21:01:34 +0530

    smooth scroll in Single-Product Component

--- src/app/single-product-page/single-product-page.component.ts
+++ src/app/single-product-page/single-product-page.component.ts
@@ -1,21 +1,19 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { NewProductsComponent } from '../new-products/new-products.component';
+
 
 @Component({
   selector: 'app-single-product-page',
   standalone: true,
-  imports: [],
+  imports: [NewProductsComponent],
   templateUrl: './single-product-page.component.html',
   styleUrl: './single-product-page.component.css'
 })
 export class SingleProductPageComponent {
 
-    constructor(private router: Router) {}
-    ngOnInit(): void {
-        this.router.events.subscribe((event) => {
-          if (event instanceof NavigationEnd) {
-            window.scrollTo(0, 0); // Scroll to the top of the page
-          }
-        });
-      }
+    constructor(private router: Router) {
+      window.scrollTo(0, 0); // Scroll to the top of the page
+    }
+
 }
