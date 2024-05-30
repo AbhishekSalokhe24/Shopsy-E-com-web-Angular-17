e303fb64393a879a73b9d1cc46f7def68a68ded8
AbhishekSalokhe24 <abhisaloke2424@gmail.com>
2024-05-30 15:05:34 +0530

    added skelaton loader

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,20 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink, NgxSkeletonLoaderModule],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-
   @Input() childArray: any[] | undefined;
-   
-  
-  
-  
-  
-} 
+  @Input() isLoading: boolean = true;
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../products.service';
 import { CarouselComponent } from "../carousel/carousel.component";
 import { FiltersComponent } from "../filters/filters.component";
@@ -6,54 +6,39 @@ import { CatogoryBtnsComponent } from "../catogory-btns/catogory-btns.component"
 import { CardComponent } from "../card/card.component";
 import { NavbarComponent } from "../navbar/navbar.component";
 import { FooterComponent } from "../footer/footer.component";
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.css',
-    imports: [CarouselComponent, FiltersComponent, CatogoryBtnsComponent, CardComponent, NavbarComponent, FooterComponent]
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  imports: [CarouselComponent, FiltersComponent, CatogoryBtnsComponent, CardComponent, NavbarComponent, FooterComponent, NgxSkeletonLoaderModule,CommonModule]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   products: any[] | undefined;
-    newdata: any[]|undefined;
-    newPrice: string | undefined;
-  
-    ngOnInit(): void {
-      this.getProducts();
-    } 
-     
-  
-   
-    constructor(private productSev:  ProductsService){
-      
-    }
-     // This is Passed By-- Btns Component
-    updateProducts(updatedProducts: any[]) {
-      this.products = updatedProducts;
-      
-    }
-  
-    // This is Passed By-- Filter Component 
-    productsByPrice(FpProducts: any[]) {
-      //console.log("Updated Prod by price: ",FpProducts)
-      this.products = FpProducts;
-    }
-  
-    
-  
-   
-  
-    getProducts(): void {
-      this.productSev.getProducts().subscribe((res: any) => {
-        this.products = res;
-        //console.log("Data From App component:");
-        //console.log(this.products);
-        this.products?.forEach((item) =>{
-        // console.log("Title: ", item.title);
-  
-        })
-      });
-  
-    }
+  isLoading = true;
+
+  constructor(private productSev: ProductsService) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  updateProducts(updatedProducts: any[]) {
+    this.products = updatedProducts;
+  }
+
+  productsByPrice(FpProducts: any[]) {
+    this.products = FpProducts;
+  }
+
+  getProducts(): void {
+    this.isLoading = true;  // Set loading to true before the request
+    this.productSev.getProducts().subscribe((res: any) => {
+      this.products = res;
+      this.isLoading = false;  // Set loading to false after the request completes
+    });
+  }
 }
